cb5cd53fe860d2f4dfe4a0985b99e59553981ca0
BlueOcean1117 <developer@blueocean.org.in>
2026-07-27 15:41:10 +0530

    Add request interceptor for authorization token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,5 +4,13 @@ const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: false,
 });
+API.interceptors.request.use((config) => {
+   const token = localStorage.getItem("erp_token");
+   if (token) {
+     config.headers.Authorization = `Bearer ${token}`;
+   }
+   return config;
+ });
+
 
 export default API;
